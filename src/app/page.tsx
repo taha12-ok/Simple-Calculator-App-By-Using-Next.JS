9cb63a6cf5b505b239948198b4e5bc8dd614a13b
Taha Shabbir <139618419+taha12-ok@users.noreply.github.com>
2024-09-10 00:22:57 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Calculator from "@/components/ui/calculator";
+export default function Home() {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+}
